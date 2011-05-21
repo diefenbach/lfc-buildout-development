d71380d6b538fca4a27f844baf930ac4a14c36bf
Kai Diefenbach <kai.diefenbach@iqpp.de>
2011-05-21 17:25:31 +0200

    Reverted bullshit

--- lfc_project/urls.py
+++ lfc_project/urls.py
@@ -21,11 +21,6 @@ urlpatterns = patterns('',
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(DIRNAME, "media"), 'show_indexes': True }),
 )
 
-# LFC RSS TAGS
-urlpatterns += patterns('',
-    (r'', include('lfc_rss_tags.urls')),
-)
-
 # LFC Blog
 urlpatterns += patterns("",
     (r'', include('lfc_blog.urls')),
